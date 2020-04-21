48faf831167595d6c1d214d803984345e0669f2c
Nan-sh <49915702+Nan-sh@users.noreply.github.com>
2020-04-21 22:04:04 +0800

    Add files via upload

---
+++ pic.py
@@ -0,0 +1,45 @@
+import requests
+import os
+import time
+from bs4 import BeautifulSoup
+##网址
+url = "https://www.enterdesk.com/zhuomianbizhi/fengjing/"
+try:
+    r = requests.get(url)
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    
+    soup = BeautifulSoup(r.text, "html.parser")
+    dd = soup.find_all('dd')
+    for temp in dd:
+            temp = temp.a.attrs['href']
+            temp = temp.replace('bizhi', 'download')
+            url_temp = temp.replace('.html', '')
+            try:
+                r_temp = requests.get(url_temp)
+                r_temp.raise_for_status()
+                r_temp.encoding = r_temp.apparent_encoding
+                soup_temp = BeautifulSoup(r_temp.text, "html.parser")
+                url_download = soup_temp.img['src']
+                try:
+                    pic = requests.get(url_download)
+                    pic.raise_for_status()
+                    img = pic.content
+                    with open('/opt/python_pic/python_jpg/1/tupian.jpg', 'wb') as f:
+                        f.write(img)
+                    time.sleep(600)
+                    with open('/opt/python_pic/python_jpg/1/tupian.jpg', 'wb') as f:
+                        f.write(img)
+                except:
+                    print("爬取错误")
+                    exit()
+            except:
+                print("爬取错误")
+                exit()
+                
+except:
+    print("爬取错误")
+    exit()
+
+
+
---
+++ pic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo mkdir /opt/python_pic/
+sudo mkdir /opt/python_pic/python_jpg/
+sudo mkdir /opt/python_pic/python_jpg/1
+sudo mkdir /opt/python_pic/python_jpg/2
+cp pic.py /opt/python_pic/
+sudo pip install requests
+sudo pip install beautifulsoup4
+cd /opt/python_pic
+
+for((i = 1; i = 1; i = i))
+do
+		sudo python pic.py
+done
