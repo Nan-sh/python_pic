8e758f6ff2bdbb5e4c1a0888b4e35d064291df6c
Nan-sh <49915702+Nan-sh@users.noreply.github.com>
2020-04-22 09:58:21 +0800

    Update auto_start.sh

--- auto_start.sh
+++ auto_start.sh
@@ -2,6 +2,7 @@
 sudo chmod a+x copy.sh
 sudo chmod a+x run.sh
 sudo ./copy.sh
+sudo chmod 777 pic.service
 sudo cp pic.service /etc/systemd/system
 systemctl start pic.service
 systemctl enable pic.service
