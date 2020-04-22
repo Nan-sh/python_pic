74c17d03ae18dcbb7f05a5f08b1469f455eca0d1
Nan-sh <49915702+Nan-sh@users.noreply.github.com>
2020-04-22 09:54:46 +0800

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -3,8 +3,8 @@ sudo mkdir /opt/python_pic/
 sudo mkdir /opt/python_pic/python_jpg/
 sudo mkdir /opt/python_pic/python_jpg/1
 sudo mkdir /opt/python_pic/python_jpg/2
-cp pic.py /opt/python_pic/
-cp run.sh /opt/python_pic/
+sudo cp pic.py /opt/python_pic/
+sudo cp run.sh /opt/python_pic/
 sudo pip install requests
 sudo pip install beautifulsoup4
 
