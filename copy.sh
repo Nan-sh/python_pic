b9516397d3809baae6310b94aea7e23b4b2b4059
Nan-sh <49915702+Nan-sh@users.noreply.github.com>
2020-04-22 09:49:42 +0800

    Update and rename pic.sh to copy.sh

--- copy.sh
+++ copy.sh
@@ -4,11 +4,7 @@ sudo mkdir /opt/python_pic/python_jpg/
 sudo mkdir /opt/python_pic/python_jpg/1
 sudo mkdir /opt/python_pic/python_jpg/2
 cp pic.py /opt/python_pic/
+cp run.sh /opt/python_pic/
 sudo pip install requests
 sudo pip install beautifulsoup4
-cd /opt/python_pic
 
-for((i = 1; i = 1; i = i))
-do
-		sudo python pic.py
-done
