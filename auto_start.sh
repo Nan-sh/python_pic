2e53d03c0c14c0fe24e1e243ce0b8e2f2e7d9f99
Nan-sh <49915702+Nan-sh@users.noreply.github.com>
2020-06-29 13:01:58 +0800

    Update auto_start.sh

--- auto_start.sh
+++ auto_start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+sudo mkdir /opt/python_pic/
+sudo mkdir /opt/python_pic/python_jpg/
+sudo cp pic.py /opt/python_pic/
+sudo cp run.sh /opt/python_pic/
+sudo pip install requests
+sudo pip install beautifulsoup4
 sudo chmod a+x copy.sh
 sudo chmod a+x run.sh
 chmod a+x wallpaper.sh
-sudo ./copy.sh
 sudo chmod 777 pic.service
 sudo cp pic.service /etc/systemd/system
 systemctl daemon-reload
