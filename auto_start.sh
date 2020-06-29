032c7bb592fed93c9d2c3ff1684c2626872aaef2
Nan-sh <49915702+Nan-sh@users.noreply.github.com>
2020-06-29 14:04:15 +0800

    Update auto_start.sh

--- auto_start.sh
+++ auto_start.sh
@@ -4,13 +4,13 @@ sudo mkdir /opt/python_pic/python_jpg/
 sudo cp pic.py /opt/python_pic/
 sudo cp run.sh /opt/python_pic/
 sudo cp wallpaper.sh /opt/python_pic
+sudo cp pic.service /etc/systemd/system
 sudo pip install requests
 sudo pip install beautifulsoup4
-sudo chmod a+x run.sh
-sudo chmod a+x wallpaper.sh
-sudo chmod 777 pic.service
+sudo chmod 777 /etc/systemd/system/pic.service
+sudo chmod a+x /opt/python_pic/run.sh
+sudo chmod a+x /opt/python_pic/wallpaper.sh
 sudo sh -c 'echo  "screen -dm bash -c \"/opt/python_pic/wallpaper.sh\"" >> /etc/profile'
-sudo cp pic.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start pic.service
 systemctl enable pic.service
