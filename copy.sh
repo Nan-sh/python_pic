d60dbe3cef18ed85e6ae37da61bbc2b0585212ab
Nan-sh <49915702+Nan-sh@users.noreply.github.com>
2020-06-29 13:00:12 +0800

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo mkdir /opt/python_pic/
 sudo mkdir /opt/python_pic/python_jpg/
-sudo mkdir /opt/python_pic/python_jpg/1
-sudo mkdir /opt/python_pic/python_jpg/2
 sudo cp pic.py /opt/python_pic/
 sudo cp run.sh /opt/python_pic/
 sudo pip install requests
