680d5c08284920885016860dd2ab8ce9f8fc9dac
Nan-sh <49915702+Nan-sh@users.noreply.github.com>
2020-06-29 13:04:11 +0800

    Delete copy.sh

--- copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo mkdir /opt/python_pic/
-sudo mkdir /opt/python_pic/python_jpg/
-sudo cp pic.py /opt/python_pic/
-sudo cp run.sh /opt/python_pic/
-sudo pip install requests
-sudo pip install beautifulsoup4
-
